4970221972518d400648d34754334f51970dba11
Jara Santamaria <jarasmar@gmail.com>
2020-03-30 18:19:34 +0100

    tdd a HTML string generator for a list of Notes

--- spec/noteListViewTest.js
+++ spec/noteListViewTest.js
@@ -2,14 +2,15 @@
 
 (function(exports) {
   function returnsHTMLString() {
-    var noteList = new NoteList();
-    noteList.create("New Note");
-    noteList.create("Another Note");
-console.log('hello');
-    if(noteList.generateView() !== '<ul><li><div>New Note</div></li><li><div>Another Note</div></li></ul>') {
-      throw new Error ('HTML string is not correct');
+    var noteListView = new NoteListView();
+
+    noteListView.noteList.create("New Note");
+    noteListView.noteList.create("Another Note");
+
+    if(noteListView.generateView() !== '<ul><li><div>New Note</div></li><li><div>Another Note</div></li></ul>') {
+      throw new Error('HTML string is not correct');
     }
   };
 
   returnsHTMLString();
-});
+})(this);
---
+++ src/noteListView.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function(exports) {
+  function NoteListView() {
+    this.noteList = new NoteList;
+  };
+
+  NoteListView.prototype.generateView = function() {
+    var textArray = [];
+    for (var i = 0; i < this.noteList.noteList.length; i++) {
+      textArray.push(this.noteList.noteList[i].text);
+    }
+
+    return('<ul><li><div>' + textArray.join('</div></li><li><div>') + '</div></li></ul>');
+
+  }
+
+  exports.NoteListView = NoteListView;
+})(this);
